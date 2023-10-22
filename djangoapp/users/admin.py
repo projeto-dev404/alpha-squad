0d0fcc2a6068e4c76fe2198af1a11527981f24b8
skapskap <skaprange@gmail.com>
2023-10-22 17:18:41 -0300

    refactor: user admin refatorado para gerenciamento de squads

--- djangoapp/users/admin.py
+++ djangoapp/users/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import User
+from squads.models import UsersOnSquads
+
+class UsersOnSquadsInline(admin.TabularInline):
+    model = UsersOnSquads
+
+class CustomUserAdmin(admin.ModelAdmin):
+    inlines = (UsersOnSquadsInline,)
 
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
