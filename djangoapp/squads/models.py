8adc69a0833d641ea5594755c13781167cfd5714
skapskap <skaprange@gmail.com>
2023-10-22 11:31:42 -0300

    feat: models do app squads

--- djangoapp/squads/models.py
+++ djangoapp/squads/models.py
@@ -1,3 +1,24 @@
+import uuid
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class Squad(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class UsersOnSquads(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    userRoleOnSquad = models.CharField(max_length=255)
+    squad = models.ForeignKey(Squad, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.name} on {self.squad.name}"
+
+    class Meta:
+        verbose_name_plural = "Squads"
