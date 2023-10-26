23ccaa3d99d0b01a8d62488fcbd5320fe52c5183
vitormcferreira <vitorpgmdr@gmail.com>
2023-10-25 23:46:49 -0300

    feature: CustomUserAdmin inherit from UserAdmin

--- djangoapp/users/admin.py
+++ djangoapp/users/admin.py
@@ -1,13 +1,50 @@
 from django.contrib import admin
-from .models import User
+from django.contrib.auth.admin import UserAdmin
 from squads.models import UsersOnSquads
+
+from .models import User
+
 
 class UsersOnSquadsInline(admin.TabularInline):
     model = UsersOnSquads
 
-class CustomUserAdmin(admin.ModelAdmin):
-    inlines = (UsersOnSquadsInline,)
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    inlines = (UsersOnSquadsInline,)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2'),
+            },
+        ),
+    )
+    list_display = ('email', 'name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email', 'name', 'email')
+    ordering = ()
+    filter_horizontal = (
+        'groups',
+        'user_permissions',
+    )
+
 
 admin.site.register(User, CustomUserAdmin)
